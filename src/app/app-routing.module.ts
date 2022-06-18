a57696b026e991eb37f0fa21fbcb710fec75252d
Matthew Berry <mjberry@illinois.edu>
2022-06-17 23:25:07 -0500

    reconfigured for easy deployment to github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SingleSampleViewerComponent } from './components/single-sample-viewer/single-sample-viewer.component';
@@ -11,7 +10,10 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    // `useHash: true` here for HashLocationStrategy, which simplifies deployment to github pages
+    // if switching back to PathLocationStrategy, restore the commented-out base href in index.html
+    // or add an APP_BASE_REF provider here
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
   exports: [
     RouterModule
