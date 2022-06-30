d3d3df3d861532f370e546bfb39c1afb05a03216
Matthew Berry <mjberry@illinois.edu>
2022-06-29 19:46:55 -0500

    changed to PathLocationStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,10 +15,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    // `useHash: true` here for HashLocationStrategy, which simplifies deployment to github pages
-    // if switching back to PathLocationStrategy, restore the commented-out base href in index.html
-    // or add an APP_BASE_REF provider here
-    RouterModule.forRoot(appRoutes, { useHash: true })
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [
     RouterModule
